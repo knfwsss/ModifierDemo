4bbc72c95795029dd538b0c8839838796d212a8c
ivan <ivan.b@vizbl.us>
2026-09-22 14:31:59 +0700

    Add DemoScreen with Hello Compose

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.modifierdemo
 
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -20,28 +23,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             ModifierDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    DemoScreen(Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun DemoScreen(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        "Hello Compose",
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold
     )
 }
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     ModifierDemoTheme {
-        Greeting("Android")
+        DemoScreen()
     }
 }
